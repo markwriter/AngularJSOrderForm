d26bbe6734c22cae09269a9b45012964b3b5336b
Hexsphere <ethanhanderson1@gmail.com>
2017-02-04 13:52:22 -0700

    Fixed Fatial Error with JSLint

--- appCode.js
+++ appCode.js
@@ -1,13 +1,13 @@
 /*jslint white:true */
 /*global angular */
-var app = angular.module("foodOrderApp", ['ngMaterial', 'ngMessages']);
+var app = angular.module("foodOrderApp", ["ngMaterial", "ngMessages"]);
 
-app.controller('firstController', function ($scope) {
+app.controller("firstController", function ($scope) {
     "use strict";
     
     $scope.foodOrderItems = [];
     $scope.orderTotal = 0;
-    $scope.orderTotalFormatted = '';
+    $scope.orderTotalFormatted = "";
     $scope.selectAll = false;
     $scope.itemQuantity = 1;
     $scope.itemQuantityChoices = [1, 2, 3, 4, 5];
@@ -59,12 +59,15 @@ app.controller('firstController', function ($scope) {
     };
 
     $scope.deleteItem = function (id) {
+        
         var i;
-        for (i = 0, i < $scope.foodOrderItems.length, i = +1;) {
-            if ($scope.foodOrderItems[i].id  === id) {
-                $scope.foodOrderItems.splice(i, 1);
+        for (i = 0; i < $scope.foodOrderItems.length; i = i +1) {
+            if ($scope.foodOrderItems[i].id  === id)  {
+            $scope.foodOrderItems.splice(i, 1);
+                }
+                
             }
-        }   
+           
         $scope.calculateOrderTotal();
     }
     
@@ -112,9 +115,9 @@ app.controller('firstController', function ($scope) {
         
     $scope.getBasePriceForFood = function(foodName){
         switch(foodName){
-            case 'pizza' : return 10;
-            case 'cake' : return 20;
-            case 'taco' : return 2;
+            case "pizza" : return 10;
+            case "cake" : return 20;
+            case "taco" : return 2;
         }
     }
     
@@ -145,35 +148,35 @@ app.controller('firstController', function ($scope) {
             $scope.foodOrderItems.push($scope.foodItem);    
         }
         
-        $scope.toppingFilter = '';
+        $scope.toppingFilter = "";
         $scope.toppingsList.forEach(function(item){item.selected = false;});
         $scope.calculateOrderTotal();
     }    
         
-    $scope.toppingFilter = ''; 
+    $scope.toppingFilter = ""; 
         
     $scope.toppingsList = [
-        {product: 'pizza', name: 'Peppers', price: 0.15, selected: false },
-        {product: 'pizza', name: 'Olives', price: 0.15, selected: false },
-        {product: 'pizza', name: 'Tomatoes', price: 0.15, selected: false },
-        {product: 'pizza', name: 'Broccoli', price: 0.15, selected: false },
-        {product: 'pizza', name: 'Onions', price: 0.15, selected: false },
-        {product: 'pizza', name: 'Mushrooms', price: 0.15, selected: false },
-        {product: 'pizza', name: 'Sausage', price: 0.15, selected: false },       
-        {product: 'pizza', name: 'Cheese', price: 0.10, selected: false },
+        {product: "pizza", name: "Peppers", price: 0.15, selected: false },
+        {product: "pizza", name: "Olives", price: 0.15, selected: false },
+        {product: "pizza", name: "Tomatoes", price: 0.15, selected: false },
+        {product: "pizza", name: "Broccoli", price: 0.15, selected: false },
+        {product: "pizza", name: "Onions", price: 0.15, selected: false },
+        {product: "pizza", name: "Mushrooms", price: 0.15, selected: false },
+        {product: "pizza", name: "Sausage", price: 0.15, selected: false },       
+        {product: "pizza", name: "Cheese", price: 0.10, selected: false },
         
-        {product: 'cake', name: 'Gumdrops', price: 0.12, selected: false },     
-        {product: 'cake', name: 'Jelly Beans', price: 0.12, selected: false },   
-        {product: 'cake', name: 'Strawberries', price: 0.12, selected: false },
-        {product: 'cake', name: 'Candle(s)', price: 0.35, selected: false },
+        {product: "cake", name: "Gumdrops", price: 0.12, selected: false },     
+        {product: "cake", name: "Jelly Beans", price: 0.12, selected: false },   
+        {product: "cake", name: "Strawberries", price: 0.12, selected: false },
+        {product: "cake", name: "Candle(s)", price: 0.35, selected: false },
         
-        {product: 'taco', name: 'Red Salsa', price: 0.10, selected: false },
-        {product: 'taco', name: 'Beans', price: 0.10, selected: false },
-        {product: 'taco', name: 'Green Salsa', price: 0.10, selected: false },
-        {product: 'taco', name: 'Cheese', price: 0.10, selected: false },
-        {product: 'taco', name: 'Beef', price: 0.10, selected: false },
-        {product: 'taco', name: 'Avocado', price: 0.10, selected: false },
-        {product: 'taco', name: 'Peppers', price: 0.10, selected: false },
-        {product: 'taco', name: 'Lettuce', price: 0.10, selected: false }          
+        {product: "taco", name: "Red Salsa", price: 0.10, selected: false },
+        {product: "taco", name: "Beans", price: 0.10, selected: false },
+        {product: "taco", name: "Green Salsa", price: 0.10, selected: false },
+        {product: "taco", name: "Cheese", price: 0.10, selected: false },
+        {product: "taco", name: "Beef", price: 0.10, selected: false },
+        {product: "taco", name: "Avocado", price: 0.10, selected: false },
+        {product: "taco", name: "Peppers", price: 0.10, selected: false },
+        {product: "taco", name: "Lettuce", price: 0.10, selected: false }          
     ];    
 });
